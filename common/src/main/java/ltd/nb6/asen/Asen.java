b0ad938da2d82dda5d0a623b61487ac7c2c29e91
HelloAIXIAOJI <aixiaoji2020@163.com>
2026-02-13 22:54:53 +0800

    init project

---
+++ common/src/main/java/ltd/nb6/asen/Asen.java
@@ -0,0 +1,9 @@
+package ltd.nb6.asen;
+
+public final class Asen {
+    public static final String MOD_ID = "asen";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/ltd/nb6/asen/neoforge/AsenNeoForge.java
@@ -0,0 +1,12 @@
+package ltd.nb6.asen.neoforge;
+
+import ltd.nb6.asen.Asen;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Asen.MOD_ID)
+public final class AsenNeoForge {
+    public AsenNeoForge() {
+        // Run our common setup.
+        Asen.init();
+    }
+}
